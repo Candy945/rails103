cc56cce852fde5daae09a4cb0baa9a03e104c026
Candy945 <525387153@qq.com>
2018-05-19 11:07:56 +0800

    implement groups#new/create/show/edit/update/destroy

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,47 @@
 class GroupsController < ApplicationController
   def index
     @groups = Group.all
-  end 
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new (group_params)
+    @group.save
+
+    redirect_to groups_path
+  end
+
+  def show
+    @group = Group.find(params[:id])
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+
+    @group.update(group_params)
+
+    redirect_to groups_path , notice:"Update Success"
+  end
+
+
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    flash[:alert] = "Group delete"
+    redirect_to groups_path
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:title, :description)
+  end
+
 end
