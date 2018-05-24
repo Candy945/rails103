3b6fcfb9e41b6edeb866ebbaa0f20e913bf8cab0
Candy945 <525387153@qq.com>
2018-05-24 18:21:12 +0800

    add order

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   belongs_to :group
-  validates :content, presence:true 
+  validates :content, presence:true
+
+  scope :recent, -> {order("created_at DESC")}
 end
