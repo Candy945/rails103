9d5220c4ab4c7d747bc857f4f54f057105a4ed6d
Candy945 <525387153@qq.com>
2018-05-27 09:42:28 +0800

    add authenticate_user! to join&quit action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,6 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user! , only:[:new, :create, :edit, :update, :destroy ]
-  before_action :find_group_and_check_permission, only:[:edit, :update, :destroy, :join, :quit  ]
+  before_action :authenticate_user! , only:[:new, :create, :edit, :update, :destroy, :join, :quit  ]
+  before_action :find_group_and_check_permission, only:[:edit, :update, :destroy ]
 
   def index
     @groups = Group.all
