e9330ee7567813c189058af68c3d619ad7972852
Candy945 <525387153@qq.com>
2018-05-27 11:07:52 +0800

    implement my groups

---
+++ app/controllers/account/groups_controller.rb
@@ -0,0 +1,6 @@
+class Account::GroupsController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    @groups = current_user.participated_groups
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,23 @@ Rails.application.routes.draw do
   devise_for :users
   devise_for :installs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :account do
+    resources :groups
+  end
+
+
 
   resources :groups do
     member do
       post :join
       post :quit
     end
-
     resources :posts
+
+
   end
+
+
 
   root 'groups#index'
 end
