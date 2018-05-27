2122a5b0881f6373087e5645c45b217010d506b2
Candy945 <525387153@qq.com>
2018-05-27 08:34:18 +0800

    implement is_member_of?(group)

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,6 +2,10 @@ class Group < ApplicationRecord
 
   belongs_to :user
   has_many :posts
+
+  has_many :group_relationships
+  has_many :members, through: :group_relationships, source: :user
+  
   validates :title, presence: true
 
 
